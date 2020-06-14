c8f39b71b24a28cc89498d9eb7ae6b656aa32137
Aratz M. Lasa <aratzml@opendeusto.es>
2020-06-14 11:46:08 +0200

    Fix. Reads operations from query

--- toad_influx_query/utils.py
+++ toad_influx_query/utils.py
@@ -2,6 +2,7 @@ import senml
 from aioinflux import iterpoints
 from toad_influx_query.logger import logger
 
+from toad_influx_query.influx import Query
 from toad_influx_query import influx
 
 
@@ -9,9 +10,9 @@ def parse_influx_query(topic, data) -> influx.Query:
     return influx.Query(topic, data)
 
 
-def influx_response_to_senml(database: str, measurement: str, response):
-    logger.info(f"Influx to SenML: {database}:{measurement}:{response}...")
-    senml_meaurements = [{"bn": database}]
+def influx_response_to_senml(query: Query, response):
+    logger.info(f"Influx to SenML: {query.db}:{query.measure}:{response}...")
+    senml_meaurements = [{"bn": query.db}]
     for point in iterpoints(response, lambda *x, meta: dict(zip(meta["columns"], x))):
         senml_json = {"t": point["time"]}
         # extract SenML time
@@ -26,17 +27,19 @@ def influx_response_to_senml(database: str, measurement: str, response):
             senml_json["v"] = point["value"]
         elif "v" in point:
             senml_json["v"] = point["v"]
+        elif query.operation:
+            senml_json["v"] = point[query.operation.lower()]
         else:
-            raise ValueError(f"No value in influx points: {response}")
+            raise ValueError(f"No value found in influx points: {response}")
         # extract SenML name
         if "name" in point:
-            senml_json["n"] = f"{measurement}/{point['name']}"
+            senml_json["n"] = f"{query.measure}/{point['name']}"
         elif "n" in point:
-            senml_json["n"] = f"{measurement}/{point['n']}"
+            senml_json["n"] = f"{query.measure}/{point['n']}"
         elif "id" in point:
-            senml_json["n"] = f"{measurement}/{point['id']}"
+            senml_json["n"] = f"{query.measure}/{point['id']}"
         else:
-            senml_json["n"] = measurement
+            senml_json["n"] = query.measure
         # extract SenML unit
         if "unit" in point:
             senml_json["u"] = point["unit"]
